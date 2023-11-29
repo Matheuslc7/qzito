99fa680dd95a3d3c0139e302cfaec070e3a6a406
matheuslc7 <matheus.lcandido7@gmail.com>
2023-11-29 16:05:06 -0300

    commit inicial

---
+++ app/src/main/java/com/example/orgs/modulo/Produto.kt
@@ -0,0 +1,11 @@
+package com.example.orgs.modulo
+
+import java.text.DateFormat
+
+class Produto (
+    val nome : String
+    val descrição : String
+    val valor : Number
+    val Quantidade : Number
+    val data : DateFormat
+)
--- app/src/main/java/com/example/orgs/recyclerviweradapter/ListaProdutosAdapter.kt
+++ app/src/main/java/com/example/orgs/recyclerviweradapter/ListaProdutosAdapter.kt
@@ -1,21 +1,46 @@
 package com.example.orgs.recyclerviweradapter
 
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.orgs.R
+import com.example.orgs.modulo.Produto
+import kotlin.contracts.Returns
 
-class ListaProdutosAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-    
-    
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        TODO("Not yet implemented")
+class ListaProdutosAdapter(
+    private val Context : Context,
+    private val produtos: List<Produto>
+) : RecyclerView.Adapter<ListaProdutosAdapter.ViewHolder>() {
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        fun vincula(produto: Produto) {
+            val nome = itemView.findViewById<TextView>(R.id.nome)
+            nome.text = produto.nome
+            val descrição = itemView.findViewById<TextView>(R.id.descrição)
+            descrição.text = produto.descrição
+            val valor  = itemView.findViewById<TextView>(R.id.valor)
+            valor.text = produto.valor.toString()
+
+
+        }
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater = LayoutInflater.from(Context)
+        val view = inflater.inflate(R.layout.produto_item, parent, false)
+        return ViewHolder(View)
     }
 
     override fun getItemCount(): Int {
         TODO("Not yet implemented")
     }
-
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val produto = produtos[position]
+        holder.vincula(produto)
     }
 
 }
